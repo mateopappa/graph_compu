d73d761168d4f5999ec5cdc57d0e1da34da5897d
marquitos <marcosdon28@gmail.com>
2025-10-13 21:55:24 +0000

    inicio de raycast cpu

--- src/cube.py
+++ src/cube.py
@@ -2,7 +2,7 @@ from hit import HitBox, HitBoxOBB
 import numpy as np
 import glm
 
-class Cube:
+class Cube(Model):
     def __init__(self, position=(0,0,0), rotation=(0,0,0), scale=(1,1,1), name="cube"):
         self.name = name
         self.position = glm.vec3(*position)
@@ -10,36 +10,46 @@ class Cube:
         self.scale = glm.vec3(*scale)
         self.__colision = HitBoxOBB(get_model_matrix=lambda: self.get_model_matrix())
 
-        self.vertices = np.array([
-            # posiciones        # colores
-            [-1, -1, -1,   1, 0, 0],
-            [ 1, -1, -1,   0, 1, 0],
-            [ 1,  1, -1,   0, 0, 1],
-            [-1,  1, -1,   1, 1, 0],
-            [-1, -1,  1,   0, 1, 1],
-            [ 1, -1,  1,   1, 0, 1],
-            [ 1,  1,  1,   1, 1, 1],
-            [-1,  1,  1,   0, 0, 0],
+        vertices = np.array([
+            -1, -1, 1,  1, -1, 1,  1, 1, 1,  -1, 1, 1,
+            -1, -1, -1,  1, -1, -1,  1, 1, -1,  -1, 1, -1
         ], dtype='f4')
 
-        # Array para crear el IBO: Index Buffer Object: define las caras del cubo
-        self.indices = np.array([
-            0, 1, 2,  2, 3, 0,   # atrás
-            4, 5, 6,  6, 7, 4,   # frente
-            0, 4, 7,  7, 3, 0,   # izquierda
-            1, 5, 6,  6, 2, 1,   # derecha
-            3, 2, 6,  6, 7, 3,   # arriba
-            0, 1, 5,  5, 4, 0    # abajo
+        colors = np.array([
+            1, 0, 0,  0, 1, 0,  0, 0, 1,  1, 1, 0,
+            1, 0, 1,  0, 1, 1,  1, 1, 1,  0, 0, 0
+        ], dtype='f4')
+
+        normals = np.array([
+            -1, -1, -1,  1, -1, -1,  1, 1, -1,  -1, 1, -1,
+            -1, -1, 1,  1, -1, 1,  1, 1, 1,  -1, 1, 1
+        ], dtype='f4')
+
+        texcoords = np.array([
+            0, 0,  1, 0,  1, 1,  0, 1,
+            0, 0,  1, 0,  1, 1,  0, 1
+        ], dtype='f4')
+
+        indices = np.array([
+            0, 1, 2,  2, 3, 0,
+            4, 7, 3,  3, 0, 4,
+            3, 2, 6,  6, 7, 3,
+            0, 1, 5,  5, 4, 0,
+            1, 2, 6,  6, 5, 1,
+            5, 6, 7,  7, 4, 5
         ], dtype='i4')
+
+
+        super().__init__(vertices, indices, colors, normals, texcoords)
 
     def check_hit(self, origin, direction):
         return self.__colision.check_hit(origin, direction)
+
     def get_model_matrix(self):
         model = glm.mat4(1)
         model = glm.translate(model, self.position)
-        model = glm.rotate(model, glm.radians(self.rotation.x), glm.vec3(1, 0, 0))
-        model = glm.rotate(model, glm.radians(self.rotation.y), glm.vec3(0, 1, 0))
-        model = glm.rotate(model, glm.radians(self.rotation.z), glm.vec3(0, 0, 1))
+        model = glm.rotate(model, glm.radians(self.rotation.x % 360), glm.vec3(1, 0, 0))
+        model = glm.rotate(model, glm.radians(self.rotation.y % 360), glm.vec3(0, 1, 0))
+        model = glm.rotate(model, glm.radians(self.rotation.z % 360), glm.vec3(0, 0, 1))
         model = glm.scale(model, self.scale)
-        
         return model
